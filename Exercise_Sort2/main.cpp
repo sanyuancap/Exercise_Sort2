e914f1e9c8b0666995197fc71644713bff2a42a8
sanyuancap <sanyuancap@foxmail.com>
2014-02-27 16:04:03 -0800

    Initial Commit

---
+++ Exercise_Sort2/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Exercise_Sort2
+//
+//  Created by ChengJY on 14-2-27.
+//  Copyright (c) 2014年 ChengJY. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
